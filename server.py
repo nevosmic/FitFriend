f718b160e40a30bf2ad8e3711052638dc8aec1ff
nevosmic <nevosmic@post.bgu.ac.il>
2021-07-04 10:52:15 +0300

    Motivation graph

--- server.py
+++ server.py
@@ -44,9 +44,9 @@ def welcome():
 
 @app.route('/main_page/<uuid:user_id>', methods=['GET', 'POST'])
 def main_page(user_id):
-    if "btn" in request.form:
+    if "btn_schedule" in request.form:
         if request.method == "POST":
-            return redirect(url_for('weekly_schedule', user_id=user_id))
+            return redirect(url_for('my_schedule', user_id=user_id))
     print("user_id ", user_id)
     cur = mysql.connection.cursor()
     cur.execute(f'SELECT user_name FROM my_proj.myusers WHERE user_id=%s', [user_id])
@@ -56,14 +56,11 @@ def main_page(user_id):
     cur.close()
     print('request.form', request.form)
 
-    return render_template('main_page.html', value=my_results[0])
+    return render_template('main_page.html', value=my_results[0], user_id=user_id)
 
 
-@app.route('/weekly_schedule/<uuid:user_id>', methods=['GET', 'POST'])
-def weekly_schedule(user_id):
-    #if "btn" in request.form:
-       # if request.method == "POST":
-        #    return redirect(url_for('weekly_schedule', user_id=user_id))
+@app.route('/my_schedule/<uuid:user_id>', methods=['GET', 'POST'])
+def my_schedule(user_id):
     print("user_id2 ", user_id)
     cur = mysql.connection.cursor()
     cur.execute(f'SELECT user_name FROM my_proj.myusers WHERE user_id=%s', [user_id])
@@ -85,8 +82,9 @@ def weekly_schedule(user_id):
     cur.close()
     return render_template('my_schedule.html', value=my_results2[0], value_id=user_id, events=events)
 
-@app.route('/insert_workouts_to DB', methods=['GET', 'POST'])
-def insert_to_db():
+
+@app.route('/insert workouts to DB', methods=['GET', 'POST'])
+def insert_workouts_to_db():
     if request.method == 'POST':
         content = request.json
         print("content: ", content)
@@ -106,27 +104,52 @@ def insert_to_db():
         cur.close()
         return 'in DB'
 
-@app.route('/insert_reports_to DB', methods=['GET', 'POST'])
-def insert_to_db():
+
+@app.route('/insert reports to DB', methods=['GET', 'POST'])
+def insert_reports_to_db():
     if request.method == 'POST':
         content = request.json
         print("content: ", content)
         cur = mysql.connection.cursor()
         cur.execute(
             """INSERT INTO 
-                my_proj.weekly_schedule (
+                my_proj.reports (
                     user_id,
-                    workout_name,
-                    type,
                     workout_date,
-                    start_hour,
-                    duration)
-            VALUES (%s,%s,%s,%s,%s,%s)""",
-            (content['_id'], content['workout_name'], content['workout_type'], content['workout_date'], content['start_hour'], content['duration']))
+                    personal_difficulty,
+                    workout_completion,
+                    motivation)
+            VALUES (%s,%s,%s,%s,%s)""",
+            (content['_id'], content['workout_date'], content['personal_difficulty'], content['workout_completion'], content['motivation']))
         mysql.connection.commit()
         cur.close()
         return 'in DB'
 
+
+@app.route('/motivation/<uuid:user_id>', methods=['GET', 'POST'])
+def create_motivation_chart(user_id):
+    cur = mysql.connection.cursor()
+    cur.execute(f'SELECT user_name FROM my_proj.myusers WHERE user_id=%s', [user_id])
+    user_name = cur.fetchone()
+    week = ('2021-07-04', '2021-07-10')
+    cur.execute(f'SELECT workout_date,motivation FROM my_proj.reports WHERE workout_date BETWEEN %s AND %s AND user_id=%s', (week[0], week[1], user_id))
+    my_data_results = cur.fetchall()
+    print("my_data_results: ", my_data_results)
+    chart_data = []
+    for item in my_data_results:
+        workout_date = item[0]
+        motiv = item[1]
+        chart_data.append((workout_date, motiv))
+
+    print("chart_data: ",chart_data)
+
+    mysql.connection.commit()
+    cur.close()
+
+    labels = [row[0] for row in chart_data]
+    values = [row[1] for row in chart_data]
+
+    return render_template('motivation.html', labels=labels, values=values, value_id=user_id)
 
 if __name__ == '__main__':
     app.run()
