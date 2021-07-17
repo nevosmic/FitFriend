0447f60ab5b2734d8e8d1b6eee15a7b62a17d259
nevosmic <nevosmic@post.bgu.ac.il>
2021-07-17 16:42:14 +0300

    weekly monthly current date

--- TEST.py
+++ TEST.py
@@ -1,5 +1,46 @@
+from datetime import date, timedelta, datetime
+#import datetime
+now = datetime.now() # current date and time
 
-print(len('4f10eca0-bbb4-11eb-9c8a-84fdd1c7d4ae'))
+year = now.strftime("%Y")
+print("year:", year)
+
+month = now.strftime("%m")
+print("month:", month)
+
+day = now.strftime("%d")
+print("day:", day)
+
+time = now.strftime("%H:%M:%S")
+print("time:", time)
+
+date_time = now.strftime("%m/%d/%Y")
+print("date and time:",date_time)
+#print(len('4f10eca0-bbb4-11eb-9c8a-84fdd1c7d4ae'))
+
+#today2 = datetime.date.today()
+today2 = datetime.today()
+now = datetime.now() # current date and time
+print(now)
+today = now.strftime("%Y-%m-%d")
+print('today' , today)
+day = now.strftime("%d")
+print("day:", day)
+curr_month = now.strftime("%m")
+print("month:", curr_month)
+
+days = {0: "SUN", 1: "MON", 2: "TUE",  3: "WED", 4: "THU", 5: "FRI", 6: "SAT"}
+# SUN = 0 -> SUN = 7 MON = 1 TUE = 2 WED = 3 THU = 4 FRI = 5 SAT = 6
+day_indx = (today2.weekday() + 1) % 7
+#day = days[day_indx]
+#print(day)
+i = day_indx
+start_of_week = int(day) - i
+end_of_week = int(day) + (6 - i)
+curr_week = (f'2021-{curr_month}-{start_of_week}', f'2021-{curr_month}-{end_of_week}')
+print(curr_week)
+#print('start_of_week ',days[start_of_week])
+#print('end_of_week ', days[end_of_week])
 """events: [
     { % for event in events %}
 {
--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 #  import interactionPlugin from '@fullcalendar/interaction'
 import uuid
-from datetime import date, timedelta
-from collections import Counter
+from datetime import date, timedelta, datetime
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 
-# fix
-events = []
 
 # connect to data base
 app.config['DEBUG'] = True
@@ -19,6 +16,42 @@ mysql = MySQL(app)
 
 
 # funcs
+def fetch_name_by_id(user_id, cur):
+    cur.execute(f'SELECT user_name FROM my_proj.myusers WHERE user_id=%s', [user_id])
+    user_name = cur.fetchone()
+    return user_name
+
+
+def reload_workouts(events, cur, user_id):
+    cur.execute(f'SELECT workout_name,workout_date,start_hour FROM my_proj.workouts_schedule WHERE user_id=%s',
+                [user_id])
+    workouts = cur.fetchall()
+    for workout in workouts:
+        workout_name = workout[0]
+        workout_start_hour = workout[2]
+        title = f"{workout_name} {workout_start_hour}"
+        date = workout[1]
+        events.append({'title': title, 'date': date})
+
+
+def current_week():
+    today = datetime.today()
+    now = datetime.now()  # current date and time
+    day = now.strftime("%d")
+    curr_month = now.strftime("%m")
+    #days = {"SUN": 0, "MON": 1, "TUE": 2, "WED": 3, "THU": 4, "FRI": 5, "SAT": 6}
+    days = {0: "SUN", 1: "MON", 2: "TUE", 3: "WED", 4: "THU", 5: "FRI", 6: "SAT"}
+    # SUN = 0 -> SUN = 7 MON = 1 TUE = 2 WED = 3 THU = 4 FRI = 5 SAT = 6
+    day_indx = (today.weekday() + 1) % 7
+    i = day_indx
+    start_of_week = int(day) - i
+    end_of_week = int(day) + (6 - i)
+    curr_week = (f'2021-{curr_month}-{start_of_week}', f'2021-{curr_month}-{end_of_week}')
+    return curr_week
+
+
+
+
 def calculate_percentages(pie_data):
     values = []
     total_num_of_workouts = sum(pie_data.values())
@@ -27,6 +60,17 @@ def calculate_percentages(pie_data):
         workout_type_percent = workout_type_ratio * 100
         values.append(workout_type_percent)
     return values
+
+
+def create_chart_data(my_data_results):
+    chart_data = []
+    for item in my_data_results:
+        workout_date = item[0]
+        motiv = item[1]
+        chart_data.append((workout_date, motiv))
+
+    chart_data_sorted = sorted(chart_data)
+    return chart_data_sorted
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -55,47 +99,29 @@ def main_page(user_id):
     if "btn_schedule" in request.form:
         if request.method == "POST":
             return redirect(url_for('my_schedule', user_id=user_id))
-    print("user_id ", user_id)
     cur = mysql.connection.cursor()
-    cur.execute(f'SELECT user_name FROM my_proj.myusers WHERE user_id=%s', [user_id])
-    my_results = cur.fetchone()
-    print("my results: ", my_results[0])
+    user_name = fetch_name_by_id(user_id, cur)
     mysql.connection.commit()
     cur.close()
-    print('request.form', request.form)
-
-    return render_template('main_page.html', value=my_results[0], user_id=user_id)
+    return render_template('main_page.html', value=user_name[0], user_id=user_id)
 
 
 @app.route('/my_schedule/<uuid:user_id>', methods=['GET', 'POST'])
 def my_schedule(user_id):
-    print("user_id2 ", user_id)
     cur = mysql.connection.cursor()
-    cur.execute(f'SELECT user_name FROM my_proj.myusers WHERE user_id=%s', [user_id])
-    my_results2 = cur.fetchone()
+    user_name = fetch_name_by_id(user_id, cur)
+    # reset events for this client
     events = []
-    cur.execute(f'SELECT workout_name,workout_date,start_hour FROM my_proj.workouts_schedule WHERE user_id=%s', [user_id])
-    workouts = cur.fetchall()
-    for workout in workouts:
-        workout_name = workout[0]
-        workout_start_hour = workout[2]
-        title = workout_name+" "+workout_start_hour
-        date = workout[1]
-        events.append({'title': title, 'date': date})
-
-        print("workouts: ", workouts)
-        print(events)
-
+    reload_workouts(events, cur, user_id)
     mysql.connection.commit()
     cur.close()
-    return render_template('my_schedule.html', value=my_results2[0], value_id=user_id, events=events)
+    return render_template('my_schedule.html', value=user_name[0], value_id=user_id, events=events)
 
 
 @app.route('/insert workouts to DB', methods=['GET', 'POST'])
 def insert_workouts_to_db():
     if request.method == 'POST':
         content = request.json
-        print("content: ", content)
         cur = mysql.connection.cursor()
         cur.execute(
             """INSERT INTO 
@@ -117,7 +143,6 @@ def insert_workouts_to_db():
 def insert_reports_to_db():
     if request.method == 'POST':
         content = request.json
-        print("content: ", content)
         cur = mysql.connection.cursor()
         cur.execute(
             """INSERT INTO 
@@ -138,7 +163,6 @@ def insert_reports_to_db():
 def check_if_workout_has_been_reported():
     if request.method == 'POST':
         content = request.json
-        print("check if reported content: ", content)
         cur = mysql.connection.cursor()
         cur.execute(
             """SELECT EXISTS(SELECT * FROM my_proj.reports 
@@ -146,45 +170,47 @@ def check_if_workout_has_been_reported():
                 """,
             (content['_id'], content['workout_date']))
         results = cur.fetchall()
-        print('results ', results)
-
         reported = 0
-        # if results not empty, this workout has been reported already
+        # check if the bit is on -this workout has been reported already
         if results[0][0] == 1:
             reported = 1
         mysql.connection.commit()
         cur.close()
         return str(reported)
-            # jsonify(not_reported=not_reported)
 
 
 @app.route('/motivation/<uuid:user_id>', methods=['GET', 'POST'])
 def create_motivation_chart(user_id):
     cur = mysql.connection.cursor()
-    cur.execute(f'SELECT user_name FROM my_proj.myusers WHERE user_id=%s', [user_id])
-    user_name = cur.fetchone()
-    today = date.today()
-    end_of_week = today + timedelta(6)
-    print("today ", today)
-    print("end_of_week ", end_of_week)
-    week = ('2021-07-04', '2021-07-10')
+    user_name = fetch_name_by_id(user_id, cur)
+    week = current_week()
     cur.execute(f'SELECT workout_date,motivation FROM my_proj.reports WHERE workout_date BETWEEN %s AND %s AND user_id=%s', (week[0], week[1], user_id))
-    my_data_results = cur.fetchall()
-    print("my_data_results: ", my_data_results)
-    chart_data = []
-    for item in my_data_results:
-        workout_date = item[0]
-        motiv = item[1]
-        chart_data.append((workout_date, motiv))
-
-    print("chart_data: ", chart_data)
-
+    my_data_res = cur.fetchall()
+    chart_data = create_chart_data(my_data_res)
     mysql.connection.commit()
     cur.close()
-
     labels = [row[0] for row in chart_data]
     values = [row[1] for row in chart_data]
+    return render_template('motivation.html', labels=labels, values=values, value_id=user_id, value=user_name[0])
 
+
+@app.route('/motivation-month/<uuid:user_id>', methods=['GET', 'POST'])
+def create_motivation_month_chart(user_id):
+    cur = mysql.connection.cursor()
+    user_name = fetch_name_by_id(user_id, cur)
+    now = datetime.now()  # current date and time
+    today = date_time = now.strftime("%Y-%m-%d")
+    day = now.strftime("%d")
+    curr_month = now.strftime("%m")
+    month = '_____{current_month}%'
+    # Finds any values that have "07" in the fifth' position
+    cur.execute(f'SELECT workout_date,motivation FROM my_proj.reports WHERE workout_date LIKE %s AND user_id=%s', (month, user_id))
+    my_data_results = cur.fetchall()
+    chart_data = create_chart_data(my_data_results)
+    mysql.connection.commit()
+    cur.close()
+    labels = [row[0] for row in chart_data]
+    values = [row[1] for row in chart_data]
     return render_template('motivation.html', labels=labels, values=values, value_id=user_id, value=user_name[0])
 
 
