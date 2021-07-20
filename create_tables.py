6a9ead199cc479928887485ca7ddcf076bfbfbe3
nevosmic <nevosmic@post.bgu.ac.il>
2021-07-20 14:22:18 +0300

    snapshots

--- create_tables.py
+++ create_tables.py
@@ -16,6 +16,5 @@ def create_weekly_table():
     cur = MySQL.connection.cursor()
     cur.execute("CREATE TABLE my_proj.weekly_schedule (user_id VARCHAR(255), workout_name VARCHAR(255), type VARCHAR(255), date VARCHAR(255), start_hour VARCHAR(255), duration VARCHAR(255))")
 
-
 if __name__ == '__main__':
     create_weekly_table()
